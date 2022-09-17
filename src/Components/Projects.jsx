8a4bc10dee45728c6f94659cf20d5961e5c8396f
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-09-17 14:12:40 +0530

    onemoreProject added

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../Styles/Projects.css";
 import bewakoof from "../Images/bewakoofhomepage.png";
 import myhours from "../Images/myhourshomepage.png";
+import rentomojo from "../Images/rentomojo.png";
 export default function Projects() {
   return (
     <div id="projects" className="pro">
@@ -33,6 +34,41 @@ export default function Projects() {
               <a
                 className="atag"
                 href="https://github.com/11-DEEPAK-KUMAR-11/Bewakoof_website"
+                target="_blank"
+              >
+                Gihub
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="commonContainer">
+        <img className="commonPic" src={rentomojo} alt="bewakoof.com" />
+
+        <div className="projectDetails">
+          <h1 className="projectHeading">Clone: rentomojo.com</h1>
+          <p className="projectPara">
+            India's leading rental platform brings to you a wide range of
+            products on rent.
+          </p>
+          <p className="teckStack">
+            <span className="teckSpan"> Teck Stack:</span> React | JSON-SERVER |
+            Heroku
+          </p>
+          <div className="linkingBtn">
+            <button className="live">
+              <a
+                className="atag"
+                href="https://isnt-sahnawaz7hussain-awesome.netlify.app/"
+                target="_blank"
+              >
+                Deployed
+              </a>
+            </button>
+            <button className="live">
+              <a
+                className="atag"
+                href="https://github.com/Sahnawaz7hussain/exultant-oatmeal-7307"
                 target="_blank"
               >
                 Gihub
