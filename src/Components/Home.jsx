6579fbb394dd42123d679079f79a079138971e52
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-10-11 13:37:03 +0530

    resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import profilePic from "../Images/profile.jpg";
 // import pdf from "../Images/Sahnawaz_Hussain_resume.pdf";
 // import { HiDownload } from "react-icons/fa";
-
+import { BiDownload } from "react-icons/bi";
 import "../Styles/Home.css";
 export default function Home() {
   const onButtonClick = () => {
@@ -24,7 +24,14 @@ export default function Home() {
         <h2>Sahnawaz Hussain</h2>
         <h1>Full Stack Web Developer.</h1>
         <button onClick={onButtonClick} className="resumeBtn">
-          Resume
+          Resume{" "}
+          <BiDownload
+            style={{
+              marginLeft: "8px",
+              fontSize: "20px",
+              alignmentBaseline: "0px",
+            }}
+          />
         </button>
       </div>
       <div className="profileContainer">
