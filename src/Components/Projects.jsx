4b161210c87cfc72379c912c45c2e7e5992c34be
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-10-16 13:52:36 +0530

    updated

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,11 +3,76 @@ import "../Styles/Projects.css";
 import bewakoof from "../Images/bewakoofhomepage.png";
 import myhours from "../Images/myhourshomepage.png";
 import rentomojo from "../Images/rentomojo.png";
+import hello_bonsai from "../Images/hello_bonsai.png";
 export default function Projects() {
+  const projects = [
+    {
+      image: bewakoof,
+      name: "bewakoof.com",
+      desc: " It's an ecommerce website in which user can buy clothes & kits.",
+      techStack: "HTML | CSS | Javascript",
+      github: "https://github.com/11-DEEPAK-KUMAR-11/Bewakoof_website",
+      demo: "https://papaya-dango-552c77.netlify.app/",
+    },
+    {
+      image: hello_bonsai,
+      name: "hellobonsai.com",
+      desc: "It's a freelance business management application that allows freelancers to work with clients.",
+      techStack: "HTML | CSS | Javascript",
+      github: "https://github.com/AnkitParte/astute-line-8992",
+      demo: "https://hellobonsai-deploy.vercel.app/",
+    },
+    {
+      image: myhours,
+      name: "myhours.com",
+      desc: "It's a project management website in which users are managing their projects.",
+      techStack: "React | ChakraUI | Router",
+      github: "https://github.com/Sahnawaz7hussain/annoyed-wind-8656",
+      demo: "https://annoyed-wind-8656.vercel.app/",
+    },
+  ];
   return (
     <div id="projects" className="pro">
       <h1 className="headingPro">My Projects</h1>
       <hr className="linePro" />
+      {projects.map((item) => (
+        <div className="commonContainer">
+          <img className="commonPic" src={item.image} alt={item.name} />
+          <div className="projectDetails">
+            <h1 className="projectHeading">Clone: {item.name}</h1>
+            <p className="projectPara">{item.desc}</p>
+            <p className="teckStack">
+              <span className="teckSpan">Tech Stack: </span>
+              {item.techStack}
+            </p>
+            <div className="linkingBtn">
+              <button className="live">
+                <a
+                  className="atag"
+                  href={`${item.demo}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Demo
+                </a>
+              </button>
+              <button className="live">
+                <a
+                  className="atag"
+                  href={item.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+      <div>
+        {/*
+      
       <div className="commonContainer">
         <img className="commonPic" src={bewakoof} alt="bewakoof.com" />
 
@@ -45,23 +110,22 @@ export default function Projects() {
         </div>
       </div>
       <div className="commonContainer">
-        <img className="commonPic" src={rentomojo} alt="bewakoof.com" />
+        <img className="commonPic" src={hello_bonsai} alt="hellobonsai.com" />
 
         <div className="projectDetails">
-          <h1 className="projectHeading">Clone: rentomojo.com</h1>
+          <h1 className="projectHeading">Clone: hellobonsai.com</h1>
           <p className="projectPara">
-            India's leading rental platform brings to you a wide range of
-            products on rent.
+            It's a freelance business management application that allows
+            freelancers to work with clients.
           </p>
           <p className="teckStack">
-            <span className="teckSpan"> Tech Stack:</span> React | JSON-SERVER |
-            Heroku
+            <span className="teckSpan"> Tech Stack:</span> React | Redux | Chakra-UI | Rest-API
           </p>
           <div className="linkingBtn">
             <button className="live">
               <a
                 className="atag"
-                href="https://isnt-sahnawaz7hussain-awesome.netlify.app/"
+                href="https://hellobonsai-deploy.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -71,7 +135,7 @@ export default function Projects() {
             <button className="live">
               <a
                 className="atag"
-                href="https://github.com/Sahnawaz7hussain/exultant-oatmeal-7307"
+                href="https://github.com/AnkitParte/astute-line-8992"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -87,12 +151,10 @@ export default function Projects() {
         <div className="projectDetails">
           <h1 className="projectHeading">Clone: myhours.com</h1>
           <p className="projectPara">
-            It's a project management website in which users are managing their
-            projects.
+            It's a project management website in which users are managing their projects.
           </p>
           <p className="teckStack">
-            <span className="teckSpan">Tech Stack:</span> React | ChakraUI |
-            Router
+            <span className="teckSpan">Tech Stack:</span> React | ChakraUI | Router
           </p>
           <div className="linkingBtn">
             <button className="live">
@@ -117,6 +179,8 @@ export default function Projects() {
             </button>
           </div>
         </div>
+      </div>
+    */}
       </div>
     </div>
   );
