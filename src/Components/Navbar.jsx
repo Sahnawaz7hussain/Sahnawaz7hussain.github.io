f19953cf6e0236d93e39bd8644d52cd8584073b5
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-12-30 01:12:36 +0530

    woring on navbar active linkl

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import { TbExternalLink } from "react-icons/tb";
 import "../Styles/Navbar.css";
 import profileLogo from "../Images/profile.jpg";
 export default function Navbar() {
   const [isMobile, setIsMobile] = useState(false);
-  // const [isActive, setIsActive] = useState(false);
-  // const handleNavClick = () => {
-  //   setIsActive(true);
-  // };
-  // console.log("isActive::", isActive);
+  const [navBG, setNavGB] = useState(true);
+  const [isActive, setIsActive] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 10) {
+        setNavGB(true);
+      } else {
+        setNavGB(false);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      let x = window.scrollY;
+      if (x > 0 && x < 600) {
+        setIsActive("home");
+      } else if (x > 600 && x < 1200) {
+        setIsActive("about");
+      } else if (x > 1200 && x < 2000) {
+        setIsActive("tech");
+      }
+    });
+  }, []);
+  // console.log("isActive: ", isActive);
   return (
-    <div className="nav">
+    <div className={!navBG ? "nav navColor" : "nav"}>
       <div>
         <Link to="hero" spy={true} smooth={true} offset={-40} duration={500}>
           <i>
@@ -20,19 +41,36 @@ export default function Navbar() {
           </i>
         </Link>
       </div>
-      <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+      <ul
+        className={
+          isMobile
+            ? !navBG
+              ? "nav-links-mobile navColor"
+              : "nav-links-mobile"
+            : "nav-links"
+        }
+      >
         <Link to="hero" spy={true} smooth={true} offset={-45} duration={500}>
-          <li onClick={() => setIsMobile(false)} className="tech-t">
+          <li
+            onClick={() => setIsMobile(false)}
+            className={isActive === "home" ? "tech-t isActive" : "tech-t"}
+          >
             Home
           </li>
         </Link>
         <Link to="about" spy={true} smooth={true} offset={-40} duration={500}>
-          <li onClick={() => setIsMobile(false)} className="tech-t">
+          <li
+            onClick={() => setIsMobile(false)}
+            className={isActive === "about" ? "tech-t isActive" : "tech-t"}
+          >
             About
           </li>
         </Link>
         <Link to="teck" spy={true} smooth={true} offset={-40} duration={500}>
-          <li onClick={() => setIsMobile(false)} className="tech-t">
+          <li
+            onClick={() => setIsMobile(false)}
+            className={isActive === "tech" ? "tech-t isActive" : "tech-t"}
+          >
             Tech
           </li>
         </Link>
