67b17c13c968330aff25d6da0cd478125586cb5f
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-06-04 01:43:33 +0530

    d

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+// dummy comment
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
